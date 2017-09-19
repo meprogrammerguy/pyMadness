430da2b026aaf8a0fbe1a557865db5d6ba965e2a
meprogrammerguy <b65535@gmail.com>
2017-09-19 11:57:18 -0500

    kenpom scraper done

--- scrape_kenpom.py
+++ scrape_kenpom.py
@@ -7,6 +7,7 @@ wiki = "https://kenpom.com/"
 page = urlopen(wiki)
 soup = BeautifulSoup(page, "html.parser")
 ratings_table=soup.find('table', id='ratings-table')
+
 A=[]
 B=[]
 C=[]
@@ -14,9 +15,14 @@ D=[]
 E=[]
 F=[]
 G=[]
+H=[]
+I=[]
+J=[]
+K=[]
+L=[]
+M=[]
 for row in ratings_table.findAll("tr"):
     col=row.findAll('td')
-    #import pdb; pdb.set_trace()
     if len(col)>0:
         A.append(col[0].find(text=True))
         B.append(col[1].find(text=True))
@@ -25,6 +31,12 @@ for row in ratings_table.findAll("tr"):
         E.append(col[4].find(text=True))
         F.append(col[5].find(text=True))
         G.append(col[7].find(text=True))
+        H.append(col[9].find(text=True))
+        I.append(col[11].find(text=True))
+        J.append(col[13].find(text=True))
+        K.append(col[15].find(text=True))
+        L.append(col[17].find(text=True))
+        M.append(col[19].find(text=True))
 
 df=pd.DataFrame(A,columns=['Rank'])
 df['Team']=B
@@ -33,6 +45,12 @@ df['W-L']=D
 df['AdjEM']=E
 df['AdjO']=F
 df['AdjD']=G
-print (df)
+df['AdjT']=H
+df['Luck']=I
+df['AdjEMSOS']=J
+df['OppOSOS']=K
+df['OppDSOS']=L
+df['AdjEMNCSOS']=M
+
 with open('kenpom.json', 'w') as f:
     f.write(df.to_json(orient='records', lines=True))
