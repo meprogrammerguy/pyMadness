545e4db549ff71acd68f2ee4ca4ec3cc42939ceb
meprogrammerguy <b65535@gmail.com>
2017-09-21 10:12:55 -0500

    working with fuzzy matching of teams

--- merge_teams.py
+++ merge_teams.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 import json
 import pdb
+from fuzzywuzzy import fuzz
+from fuzzywuzzy import process
 
 file = 'espn.json'
 with open(file) as espn_file:
@@ -10,3 +12,21 @@ file = 'kenpom.json'
 with open(file) as kenpom_file:
     dict_kenpom = json.load(kenpom_file)
 
+AllTeams=[]
+for item in dict_espn.values():
+    AllTeams.append(item["TeamA"])
+    AllTeams.append(item["TeamB"])
+team_set = set(AllTeams)
+espn_teams = list(team_set)
+espn_teams.sort()
+
+AllTeams=[]
+for item in  dict_kenpom.values():
+    AllTeams.append(item["Team"])
+team_set = set(AllTeams)
+kenpom_teams = list(team_set)
+kenpom_teams.sort()
+
+for item in espn_teams:
+    print (item, process.extractOne(item, kenpom_teams))
+#pdb.set_trace()
