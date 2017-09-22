e01188fd95f894e5c7413ab722a84fb573cdd704
meprogrammerguy <b65535@gmail.com>
2017-09-22 10:55:31 -0500

    playing with xml files

--- merge_teams.py
+++ merge_teams.py
@@ -3,6 +3,10 @@ import json
 import pdb
 from fuzzywuzzy import fuzz
 from fuzzywuzzy import process
+import lxml.etree
+import lxml.builder    
+from lxml import etree
+from lxml import etree as ET
 
 file = 'espn.json'
 with open(file) as espn_file:
@@ -27,6 +31,16 @@ team_set = set(AllTeams)
 kenpom_teams = list(team_set)
 kenpom_teams.sort()
 
-for item in espn_teams:
-    print (item, process.extractOne(item, kenpom_teams))
+#for item in espn_teams:
+#    print (item, process.extractOne(item, kenpom_teams))
+
+
 #pdb.set_trace()
+
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+tree.write("merge.xml", pretty_print=True)
