2d8f01bd523460a1bba65998486221d264849dfc
meprogrammerguy <b65535@gmail.com>
2017-09-26 18:44:54 -0500

    write linux shell test

---
+++ test/linux/gonzaga_villanova_neutral.sh
@@ -0,0 +1,5 @@
+# Test #1 Gonzaga vs. Villanova at a neutral venue
+# 
+#  https://gamepredict.us/kenpom?team_a=Gonzaga&team_b=Villanova&neutral=true
+#
+./score_match.py --first="Gonzaga" --second="Villanova" --neutral
