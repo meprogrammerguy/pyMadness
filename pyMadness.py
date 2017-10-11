1d2518e38ae1894cf4cdb900eb6509a24ce624dc
meprogrammerguy <b65535@gmail.com>
2017-10-11 11:42:45 -0500

    test routines done

--- pyMadness.py
+++ pyMadness.py
@@ -35,27 +35,15 @@ def findTeams(first, second, verbose = True, file = "kenpom.json"):
     return teama, teamb
 
 def Chance(teama, teamb, std = 11, homeAdvantage = 3.75, homeTeam = 'none', verbose = True):
-    Tdiff = (float(teama['AdjT']) + float(teamb['AdjT'])) / 200.0
+    EffMgn = Line(teama, teamb, verbose = False, homeTeam = homeTeam, homeAdvantage = homeAdvantage)
     if (verbose):
-        print ("Chance(tempo) {0}".format(Tdiff * 100))
-    EMdiff = (float(teama['AdjEM']) - float(teamb['AdjEM'])) * Tdiff
-    if homeTeam == teama["Team"]:
-        bPercent = norm.cdf(0, EMdiff + homeAdvantage, std)
-        if (verbose):
-            print ("Chance(efficiency margin) {0}".format(EMdiff + homeAdvantage))
-    elif homeTeam == teamb["Team"]:
-        bPercent = norm.cdf(0, EMdiff - homeAdvantage, std)
-        if (verbose):
-            print ("Chance(efficiency margin) {0}".format(EMdiff - homeAdvantage))
-    else:
-        bPercent = norm.cdf(0, EMdiff, std)
-        if (verbose):
-            print ("Chance(efficiency margin) {0}".format(EMdiff))
+        print ("Chance(efficiency margin) {0}".format(EffMgn))
+    bPercent = norm.cdf(0, EffMgn, std)
     aPercent = 1.0 - bPercent
     aPercent = int(round(aPercent * 100.0))
     bPercent = int(round(bPercent * 100.0))
     if (verbose):
-        print ("Chance(teama) {0}".format(aPercent), "Chance(teamb) {0}".format(bPercent))
+        print ("Chance({0}) {1}%".format(teama["Team"], aPercent), "vs. Chance({0}) {1}%".format(teamb["Team"], bPercent))
     return aPercent, bPercent
 
 def Tempo(teama, teamb, verbose = True):
@@ -95,14 +83,23 @@ def Test(verbose):
     return False
 
 def Score(teama, teamb, verbose = True, homeAdvantage = 3.75, homeTeam = 'none'):
-    # Number of points A should win by on neutral floor
-    #kpEMdiff = (kpEMa - kpEMb)*kpEMtempo
-    # Number of points A should win by on oponents floor
-    #kpEMdiff = (kpEMa - kpEMb)*kpEMtempo - homeadv
-    Tdiff = (float(teama['AdjT']) + float(teamb['AdjT'])) / 200.0
+    tempo = Tempo(teama, teamb, False)
     if (verbose):
-        print ("Score(tempo) {0}".format(Tdiff * 100))
-    EMdiff = (float(teama['AdjEM']) - float(teamb['AdjEM'])) * Tdiff
+        print ("Score(tempo) {0}".format(tempo * 100))
+    EffMgn = Line(teama, teamb, verbose = False, homeTeam = homeTeam, homeAdvantage = homeAdvantage)
+    if (verbose):
+        print ("Score(efficiency margin) {0}".format(EffMgn))
+    aScore = int(round((tempo * 100) + (EffMgn / 2.0)))
+    bScore = int(round((tempo * 100) - (EffMgn / 2.0)))
+    if (verbose):
+        print ("Score({0}) {1}".format(teama["Team"], aScore), "vs. Score({0}) {1}".format(teamb["Team"], bScore))
+    return aScore, bScore
+
+def Line(teama, teamb, verbose = True, homeAdvantage = 3.75, homeTeam = 'none'):
+    tempo = Tempo(teama, teamb, False)
+    if (verbose):
+        print ("Line(tempo) {0}".format(tempo * 100))
+    EMdiff = (float(teama['AdjEM']) - float(teamb['AdjEM'])) * tempo
     EffMgn = 0
     if homeTeam == teama["Team"]:
         EffMgn = EMdiff + homeAdvantage
@@ -111,13 +108,8 @@ def Score(teama, teamb, verbose = True, homeAdvantage = 3.75, homeTeam = 'none')
     else:
         EffMgn = EMdiff
     if (verbose):
-        print ("Score(efficiency margin) {0}".format(EffMgn))
-    #pdb.set_trace()
-    aScore = int(round((Tdiff * 100) + (EffMgn / 2.0)))
-    bScore = int(round((Tdiff * 100) - (EffMgn / 2.0)))
-    if (verbose):
-        print ("Score(teama) {0}".format(aScore), "Score(teamb) {0}".format(bScore))
-    return aScore, bScore
+        print ("Line(efficiency margin) {0}".format(EffMgn))
+    return EffMgn
 
 def Calculate(first, second, neutral, verbose):
     if (verbose):
@@ -133,14 +125,18 @@ def Calculate(first, second, neutral, verbose):
         return {}
     if (not neutral):
         chancea, chanceb =  Chance(teama, teamb, homeTeam = teamb["Team"], verbose = verbose)
+        scorea, scoreb = Score(teama, teamb, verbose = verbose, homeTeam = teamb["Team"])
+        line = Line(teama, teamb, verbose = verbose, homeTeam = teamb["Team"])
     else:
         chancea, chanceb =  Chance(teama, teamb, verbose = verbose)
+        scorea, scoreb = Score(teama, teamb, verbose = verbose)
+        line = Line(teama, teamb, verbose = verbose)
+
     tempo = Tempo(teama, teamb, verbose = verbose)
-    scorea, scoreb = Score(teama, teamb, verbose = verbose)
     #pdb.set_trace()
 
-    dict_score = {'teama':first, 'scorea':"{0}".format(int(round(scorea))), 'chancea':"{0}%".format(int(round(chancea * 100))) ,'teamb':second, 'scoreb':"{0}".format(int(round(scoreb))), 'chanceb':"{0}%".format(int(round(chanceb * 100))), 'line':1, 'tempo':"{0}".format(int(round(tempo * 100))) }
+    dict_score = {'teama':first, 'scorea':"{0}".format(scorea), 'chancea':"{0}%".format(chancea) ,'teamb':second, 'scoreb':"{0}".format(scoreb), 'chanceb':"{0}%".format(chanceb), 'line':int(round(line)), 'tempo':"{0}".format(int(round(tempo * 100))) }
     if (verbose):
         print ("Calculate(dict_score) {0}".format(dict_score))
-    pdb.set_trace()
+    #pdb.set_trace()
     return dict_score
