705b17f58024fcd5fcf3c5361c5bd508f7c0f129
meprogrammerguy <b65535@gmail.com>
2024-07-24 16:04:00 -0500

    score_matchup works, test routine works, always verbose

--- pyMadness.py
+++ pyMadness.py
@@ -8,7 +8,7 @@ import pdb
 from scipy.stats import norm
 from collections import OrderedDict
 
-def findTeams(first, second, verbose = True, file = "stats.json"):
+def findTeams(first, second, file = "json/stats.json"):
     teama = {}
     teamb = {}
     count = 0
@@ -33,104 +33,93 @@ def findTeams(first, second, verbose = True, file = "stats.json"):
         return {}, {}
     return teama, teamb
 
-def Chance(teama, teamb, std = 11, homeAdvantage = 3.75, homeTeam = 'none', verbose = True):
-    EffMgn = Line(teama, teamb, verbose = False, homeTeam = homeTeam, homeAdvantage = homeAdvantage)
-    if (verbose):
-        print ("Chance(efficiency margin) {0}".format(EffMgn))
-    bPercent = norm.cdf(0, EffMgn, std)
+def Chance(teama, teamb, neutral):
+    EffMgn = Line(teama, teamb, neutral)
+    print ("Chance(efficiency margin) {0}".format(EffMgn))
+    bPercent = norm.cdf(0, EffMgn, 11)
     aPercent = 1.0 - bPercent
     aPercent = int(round(aPercent * 100.0))
     bPercent = int(round(bPercent * 100.0))
-    if (verbose):
-        print ("Chance({0}) {1}%".format(teama["Team"], aPercent), "vs. Chance({0}) {1}%".format(teamb["Team"], bPercent))
+    print ("Chance({0}) {1}%".format(teama["Team"], aPercent), "vs. Chance({0}) {1}%".format(teamb["Team"], bPercent))
     return aPercent, bPercent
 
-def Tempo(teama, teamb, verbose = True):
+def Tempo(teama, teamb):
     Tdiff = (float(teama['AdjT']) + float(teamb['AdjT'])) / 200.0
-    if (verbose):
-        print ("Tempo(tempo) {0}".format(Tdiff * 100))
+    print ("Tempo(tempo) {0}".format(Tdiff * 100))
     return Tdiff
 
-def Test(verbose):
+def Test():
     result = 0
     # Purdue, Northwestern on 3/5/17
     # Actual Score: Edwards leads No. 16 Purdue past Northwestern, 69-65
     # venue was: Welsh-Ryan Arena in Evanston, IL (Northwestern was the home team)
-    teama = {'Team':"purdue", 'AdjEM':24.31, 'AdjT':69.5, 'Result1':58, 'Result2':69}
-    teamb = {'Team':"northwestern", 'AdjEM':15.83, 'AdjT':65.8, 'Result1':42,'Result2':67}
-    if (verbose):
-        print ("Test #1 Purdue at Northwestern on 3/5/17")
-        print ("        Northwestern is Home team, testing Chance() routine)")
-    chancea, chanceb =  Chance(teama, teamb, homeTeam = teamb["Team"], verbose = verbose, homeAdvantage = 3.5)
+    teama = {'Team':"purdue", 'AdjEM':24.31, 'AdjT':69.5, 'Result1':81, 'Result2':72}
+    teamb = {'Team':"northwestern", 'AdjEM':15.83, 'AdjT':65.8, 'Result1':19,'Result2':63}
+    print (teama)
+    print (teamb)
+    print ("Test #1 Purdue at Northwestern on 3/5/17")
+    print ("        Northwestern is Home team, testing Chance() routine)")
+    chancea, chanceb =  Chance(teama, teamb, False)
     if (teama['Result1'] == chancea):
         result += 1
     if (teamb['Result1'] == chanceb):
         result += 1
-    if (verbose and result == 2):
+    if (result == 2):
         print ("Test #1 - pass")
-    if (verbose):
-        print ("Test #2 Purdue at Northwestern on 3/5/17")
-        print ("        Northwestern is Home team, testing Score() routine)")
-    scorea, scoreb = Score(teama, teamb, verbose = verbose, homeTeam = teamb["Team"], homeAdvantage = 3.5)
+    print ("Test #2 Purdue at Northwestern on 3/5/17")
+    print ("        Northwestern is Home team, testing Score() routine)")
+    print ("                    Actual Score: Edwards lead No. 16 Purdue past Northwestern, 69-65")
+    scorea, scoreb = Score(teama, teamb, False)
     if (teama['Result2'] == scorea):
         result += 1
     if (teamb['Result2'] == scoreb):
         result += 1
     if (result == 4):
+        print ("Test #2 - pass")
         return True
     return False
 
-def Score(teama, teamb, verbose = True, homeAdvantage = 3.75, homeTeam = 'none'):
-    tempo = Tempo(teama, teamb, False)
-    if (verbose):
-        print ("Score(tempo) {0}".format(tempo * 100))
-    EffMgn = Line(teama, teamb, verbose = False, homeTeam = homeTeam, homeAdvantage = homeAdvantage)
-    if (verbose):
-        print ("Score(efficiency margin) {0}".format(EffMgn))
+def Score(teama, teamb, neutral):
+    tempo = Tempo(teama, teamb)
+    EffMgn = Line(teama, teamb, neutral)
+    print ("Score(efficiency margin) {0}".format(EffMgn))
     aScore = int(round((tempo * 100) + (EffMgn / 2.0)))
     bScore = int(round((tempo * 100) - (EffMgn / 2.0)))
-    if (verbose):
-        print ("Score({0}) {1}".format(teama["Team"], aScore), "vs. Score({0}) {1}".format(teamb["Team"], bScore))
+    print ("Score({0}) {1}".format(teama["Team"], aScore), "vs. Score({0}) {1}".format(teamb["Team"], bScore))
     return aScore, bScore
 
-def Line(teama, teamb, verbose = True, homeAdvantage = 3.75, homeTeam = 'none'):
-    tempo = Tempo(teama, teamb, False)
-    if (verbose):
-        print ("Line(tempo) {0}".format(tempo * 100))
+def Line(teama, teamb, neutral):
+    tempo = Tempo(teama, teamb)
+    print ("Line(tempo) {0}".format(tempo * 100))
     EMdiff = (float(teama['AdjEM']) - float(teamb['AdjEM'])) * tempo
     EffMgn = 0
-    if homeTeam == teama["Team"]:
-        EffMgn = EMdiff + homeAdvantage
-    elif homeTeam == teamb["Team"]:
-        EffMgn = EMdiff - homeAdvantage
-    else:
+    if neutral:
         EffMgn = EMdiff
-    if (verbose):
-        print ("Line(efficiency margin) {0}".format(EffMgn))
+    else:
+        EffMgn = EMdiff + 3.75
+    print ("Line(efficiency margin) {0}".format(EffMgn))
     return EffMgn
 
-def Calculate(first, second, neutral, verbose):
-    if (verbose):
-        if (neutral):
-            info = "{0} verses {1} at a neutral location".format(first, second)
-            print (info)
-        else:
-            info = "Visiting team: {0} verses Home team: {1}".format(first, second)
-            print (info)
+def Calculate(first, second, neutral):
+    if (neutral):
+        info = "{0} verses {1} at a neutral location".format(first, second)
+        print (info)
+    else:
+        info = "Visiting team: {0} verses Home team: {1}".format(first, second)
+        print (info)
 
-    teama, teamb = findTeams(first, second, verbose = verbose)
+    teama, teamb = findTeams(first, second)
     if (not teama or not teamb):
         return {}
-    if (not neutral):
-        chancea, chanceb =  Chance(teama, teamb, homeTeam = teamb["Team"], verbose = verbose)
-        scorea, scoreb = Score(teama, teamb, verbose = verbose, homeTeam = teamb["Team"])
-        line = Line(teama, teamb, verbose = verbose, homeTeam = teamb["Team"])
-    else:
-        chancea, chanceb =  Chance(teama, teamb, verbose = verbose)
-        scorea, scoreb = Score(teama, teamb, verbose = verbose)
-        line = Line(teama, teamb, verbose = verbose)
-    tempo = Tempo(teama, teamb, verbose = verbose)
-    dict_score = {'teama':first, 'scorea':"{0}".format(scorea), 'chancea':"{0}%".format(chancea) ,'teamb':second, 'scoreb':"{0}".format(scoreb), 'chanceb':"{0}%".format(chanceb), 'line':int(round(line)), 'tempo':"{0}".format(int(round(tempo * 100))) }
-    if (verbose):
-        print ("Calculate(dict_score) {0}".format(dict_score))
+    chancea, chanceb =  Chance(teama, teamb, neutral)
+    scorea, scoreb = Score(teama, teamb, neutral)
+    line = Line(teama, teamb, neutral)
+    tempo = Tempo(teama, teamb)
+    dict_score = \
+    {
+        'teama':first, 'scorea':"{0}".format(scorea), 'chancea':"{0}%".format(chancea) \
+        ,'teamb':second, 'scoreb':"{0}".format(scoreb), 'chanceb':"{0}%".format(chanceb), \
+        'line':int(round(line)), 'tempo':"{0}".format(int(round(tempo * 100))) \
+    }
+    print ("Calculate(dict_score) {0}".format(dict_score))
     return dict_score
--- score_matchup.py
+++ score_matchup.py
@@ -20,21 +20,17 @@ def RefreshStats():
 def main(argv):
     first = ""
     second = ""
-    verbose = False
     neutral = False
     test = False
     try:
-        opts, args = getopt.getopt(argv, "hf:s:vnt", ["help", "first=", "second=", "verbose", "neutral","test"])
+        opts, args = getopt.getopt(argv, "hf:snt", ["help", "first=", "second=", "neutral","test"])
     except getopt.GetoptError as err:
         print(err)
         usage()
         sys.exit(2)
     output = None
-    verbose = False
     for o, a in opts:
-        if o in ("-v", "--verbose"):
-            verbose = True
-        elif o in ("-n", "--neutral"):
+        if o in ("-n", "--neutral"):
             neutral = True
         elif o in ("-t", "--test"):
             test = True
@@ -49,11 +45,10 @@ def main(argv):
             assert False, "unhandled option"
     print ("Score Matchup Tool")
     print ("**************************")
-    if (verbose):
-        usage()
-        print ("**************************")
+    usage()
+    print ("**************************")
     if (test):
-        testResult = pyMadness.Test(verbose)
+        testResult = pyMadness.Test()
         if (testResult):
             print ("Test result - pass")
         else:
@@ -62,11 +57,11 @@ def main(argv):
         if (not first and not second):
             print ("you must input the team names to run this tool, (first and second arguments)")
             exit()
-        file = "stats.json"
+        file = "json/stats.json"
         if (not CurrentStatsFile(file)):
             RefreshStats()
         ds = {}
-        ds = pyMadness.Calculate(first, second, neutral, verbose)
+        ds = pyMadness.Calculate(first, second, neutral)
         if (not ds):
             print ("Could not predict this matchup?")
             exit()
@@ -80,7 +75,6 @@ def main(argv):
 def usage():
     usage = """
     -h --help                 Prints this
-    -v --verbose              Increases the information level
     -f --first                First Team (The Away Team)
     -s --second               Second Team (The Home Team)
     -n --neutral              Playing on a neutral Field
